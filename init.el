110c286d9ed75d57dd68553c4b1a70445b95c56d
Reza <rezaqodsi77@gmail.com>
2020-08-19 13:12:57 +0430

    added solorized theme and bidi support for org-mode

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,7 +14,7 @@
  '(custom-safe-themes
    (quote
     ("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default)))
- '(package-selected-packages (quote (solarized-theme evil))))
+ '(package-selected-packages (quote (twittering-mode web-mode solarized-theme evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,3 +26,7 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(defun set-bidi-env ()
+  "interactive"
+  (setq bidi-paragraph-direction 'nil))
+(add-hook 'org-mode-hook 'set-bidi-env)
